21e8a2313c1f63aab126e501d0b12fea8af009fc
Frank Seiffert <frank.seiffert@ebee.de>
2023-06-30 08:20:29 +0200

    first try

---
+++ scripts/scan-java.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+printf "" > /tmp/java-$(hostname).txt
+
+for f in $(find / -xdev -name java -type f ! -size 0 -exec grep -IL . "{}" \;); do
+  printf "java;" >> /tmp/java-$(hostname).txt
+  dirname $f | tr -d "\n" >> /tmp/java-$(hostname).txt
+  printf ";" >> /tmp/java-$(hostname).txt
+  $f -version 2> /tmp/java-$(hostname)-binary.txt
+  grep -m1 -i version /tmp/java-$(hostname)-binary.txt | sed 's/.*"\(.*\)".*/\1/g' | tr -d "\n" >> /tmp/java-$(hostname).txt
+  
+  sed -i '/version/d' /tmp/java-$(hostname)-binary.txt 
+  sed -i '/Server/d' /tmp/java-$(hostname)-binary.txt 
+  cat /tmp/java-$(hostname)-binary.txt | tr -d "\n" >> /tmp/java-$(hostname).txt
+  
+  echo ";NTS\\"$(id -un) >> /tmp/java-$(hostname).txt
+done
+
+
